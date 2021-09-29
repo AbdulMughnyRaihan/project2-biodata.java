96f4034b497cb079b68ad9327e1be09a5bb2d749
AbdulMughnyRaihan <abdulmughnyraihan@gmail.com>
2021-09-29 09:48:55 +0700

    Biodata.java

---
+++ NotePad++/Biodata.java
@@ -0,0 +1,13 @@
+public class Biodata{
+
+public static void main(String[] args){
+    System.out.println ("______________<>_____________");
+    System.out.println ("  Nama : Abdul MUghny Raihan");
+    System.out.println ("  NIM  : 6706210114");
+    System.out.println ("  Kelas: 45-01");
+    System.out.println ("______________<>_____________");
+    }
+
+}
+	 
+	 
